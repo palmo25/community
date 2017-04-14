a4b0cff9870b32b118183bc5314f5cfb16420356
Sandro Palmieri <sandropalmieri@outlook.com>
2017-04-14 21:47:47 +0200

    added dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   resources :members
   root "admin#index"
+  get'/dashboard', to: "dashboard#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
